4fb68397ae132927f52af36e43c970b75c6f820e
kaiyuanlee3641 <kaiyuanlee@umass.edu>
2021-11-06 13:09:14 -0400

    Updated App.py with basic gpiozero

--- App.py
+++ App.py
@@ -1,5 +1,74 @@
 import Weather
 import Message
 
-# print(Message.sendMessage("Message test from Twilio.", '+14134064010'))
+from gpiozero import Motor
+from time import sleep
+
+in1 = 24
+in2 = 23
+en = 25
+temp1=1
+
+frontMotors = (in1, in2)
+
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(in1,GPIO.OUT)
+# GPIO.setup(in2,GPIO.OUT)
+# GPIO.setup(en,GPIO.OUT)
+# GPIO.output(in1,GPIO.LOW)
+# GPIO.output(in2,GPIO.LOW)
+# p=GPIO.PWM(en,1000)
+
+# p.start(25)
+print("\n")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")    
+
+speed = 1
+
+while(1):
+
+    x=raw_input()
+    
+    if x=='r':
+        print("run")
+        if(temp1==1):
+         frontMotors.forward(speed)
+         print("forward")
+         x='z'
+        else:
+         frontMotors.backward(speed)
+         print("backward")
+         x='z'
+
+
+    elif x=='s':
+        print("stop")
+        frontMotors.stop()
+        x='z'
+
+    elif x=='f':
+        print("forward")
+        frontMotors.forward(speed)
+        temp1=1
+        x='z'
+
+    elif x=='b':
+        print("backward")
+        frontMotors.backward(speed)
+        temp1=0
+        x='z'
+
+    elif x=='-':
+        speed-=0.2
+    
+    elif x=='+':
+        speed+=0.2
+
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
+
+# print(Message.sendMessage(""))
 # print(Weather.getCurrentWeather())
--- Message.py
+++ Message.py
@@ -2,14 +2,14 @@ import os
 from twilio.rest import Client
 
 account_sid = 'AC68cb445f7aee74876779d2c93748b227'
-auth_token = 'd2420dbcd9cc3d2ae8eb4b697fe48cdc'
+auth_token = '8f1a118cdbc10768d8761b0e63e262f5'
 client = Client(account_sid, auth_token)
 
-def sendMessage(text, outgoing):
+def sendMessage(text):
     message = client.messages \
                     .create(
                         body=text,
                         from_='+13082183325',
-                        to=outgoing
+                        to='+14134064010'
                     )
     print(message.sid)
