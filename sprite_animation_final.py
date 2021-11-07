08e8a984f637cea2d2b924940bfa6f0bc9a0aa7a
kaiyuanlee3641 <kaiyuanlee@umass.edu>
2021-11-07 01:05:19 -0400

    move center left

--- sprite_animation_final.py
+++ sprite_animation_final.py
@@ -20,7 +20,7 @@ class Player(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         #self.rect.topleft = [pos_x, pos_y]
-        self.rect.center = (400, 240)
+        self.rect.center = (325, 240)
 
     def attack(self):
         self.current_sprite = 0
