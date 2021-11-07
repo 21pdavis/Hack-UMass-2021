8626cef35fb8f915562c241e1c27a5d2e1c99192
21pdavis <paulcdavis135@gmail.com>
2021-11-07 02:35:54 -0500

    First iteration of update to TTS

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -1,12 +1,20 @@
-from gtts import gTTS
-  
-# This module is imported so that we can 
-# play the converted audio
-import os
+# from gtts import gTTS
+import pyttsx3
+import random
+# import os
 
-def play(mytext):
-    language = 'en'
-    myobj = gTTS(text=mytext, lang=language, slow=False)
-    myobj.save("texttospeech.mp3")
-    os.system("texttospeech.mp3")
-    os.remove("./texttospeech.mp3")
+def play():
+    engine = pyttsx3.init()
+
+    with open('pickup_lines.txt') as file:
+        lines = file.readlines()
+
+    rand_int = random.randrange(1, len(lines))
+    print(lines[rand_int])
+    engine.say(lines[rand_int])
+    engine.runAndWait()
+    # language = 'en'
+    # myobj = gTTS(text=mytext, lang=language, slow=False)
+    # myobj.save("texttospeech.mp3")
+    # os.system("texttospeech.mp3")
+    # os.remove("./texttospeech.mp3")
